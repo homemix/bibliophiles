9e690d07121a9f45e700267adea57658b7bb41c2
homemix <kenmutati@gmail.com>
2022-05-10 10:43:55 +0300

    set up books view datatable

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return render_template('base.html')
+
+
+@app.route('/books')
+def view_books():
+    page_title = 'Books'
+    return render_template('books/index.html', page_title=page_title)
 
 
 if __name__ == '__main__':
