564b3dbda50faa2c9b0e5d954a5addb65db59baf
homemix <kenmutati@gmail.com>
2022-05-10 17:44:42 +0300

    set up books models

--- models/Books.py
+++ models/Books.py
@@ -1,7 +1,25 @@
 from models.database import db
+
 """ A class to represent a book """
+
 
 class Books(db.Model):
     """ Books Model """
     __tablename__ = 'books'
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    author = db.Column(db.String(80), nullable=False)
+    published_date = db.Column(db.Date, nullable=False)
+    publisher = db.Column(db.String(80), nullable=False)
+    genre_id = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, title, author, genre_id, published_date, publisher):
+        self.title = title
+        self.author = author
+        self.genre_id = genre_id
+        self.published_date = published_date
+        self.publisher = publisher
+
+    def __repr__(self):
+        return "<Books %r>" % self.title, self.author, self.genre_id, self.published_date, self.publisher
+
