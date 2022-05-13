423a0c6886114585a6af2cdcc73492dffeb69110
homemix <kenmutati@gmail.com>
2022-05-13 09:53:09 +0300

    fix Reviews merge conflict

--- models/Reviews.py
+++ models/Reviews.py
@@ -9,19 +9,10 @@ class Reviews(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ratings = db.Column(db.Integer, nullable=False)
     reviews = db.Column(db.Text(), nullable=False)
-<<<<<<< HEAD
-    
     books_id = db.Column(db.Integer, db.ForeignKey('Books.id'))
     books = db.relationship("Books", back_populates="reviews")
-
     userId = db.Column(db.Integer, db.ForeignKey('Users.id'))
-=======
-    books_id = db.Column(db.Integer, db.ForeignKey('UserTypes.id'))
-    Reviews = db.relationship("Books", back_populates="Reviews")
-
-    user_id = db.Column(db.Integer, db.ForeignKey('UserTypes.id'))
->>>>>>> 648a61c527909ebf4ecf9c9b75d5a83b35944ddc
-    user_id = db.relationship("Users", back_populates="reviews")
+    users = db.relationship("Users", back_populates="reviews")
 
     def __init__(self, ratings, reviews, books_id, user_id):
         self.books_id = books_id
